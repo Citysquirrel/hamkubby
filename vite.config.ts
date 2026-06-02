262fbbaf6070c6536848196b0671572f8cfb6a12
citysquirrel <tok1324@naver.com>
2026-06-02 15:08:37 +0900

    빌드 옵션 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,66 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ssl from "@vitejs/plugin-basic-ssl";
+import viteCompression from "vite-plugin-compression"; // 압축 플러그인 (npm i -D vite-plugin-compression)
 
-// https://vite.dev/config/
-export default defineConfig({
-	server: {
-		proxy: {
-			"/api": "http://localhost:8788",
+export default defineConfig(({ mode }) => {
+	const isProd = mode === "production";
+
+	return {
+		plugins: [
+			react(),
+			!isProd && ssl(),
+			isProd &&
+				viteCompression({
+					algorithm: "gzip",
+					ext: ".gz",
+				}),
+			isProd &&
+				viteCompression({
+					algorithm: "brotliCompress",
+					ext: ".br",
+				}),
+		].filter(Boolean),
+
+		server: {
+			proxy: {
+				"/api": "http://localhost:8788",
+			},
 		},
-	},
-	plugins: [react()],
+
+		esbuild: {
+			drop: isProd ? ["console", "debugger"] : [],
+		},
+
+		build: {
+			sourcemap: false,
+
+			assetsInlineLimit: 2048,
+
+			rollupOptions: {
+				output: {
+					manualChunks: (id) => {
+						if (id.includes("node_modules")) {
+							if (id.includes("@chakra-ui")) {
+								return "vendor-chakra";
+							}
+							if (id.includes("recharts")) {
+								return "vendor-recharts";
+							}
+							if (id.includes("react-icons")) {
+								return "vendor-icons";
+							}
+							if (id.includes("lodash")) {
+								return "vendor-lodash";
+							}
+						}
+					},
+					entryFileNames: "[name].[hash].js",
+					chunkFileNames: "chunks/[name].[hash].js",
+					assetFileNames: "assets/[name].[hash].[ext]",
+				},
+			},
+		},
+	};
 });
 
