0d95da6a906b5343342064ac653613f35270fad5
citysquirrel <tok1324@naver.com>
2026-07-04 23:51:55 +0900

    toast에 잘못 입혀진 bg 삭제

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -14,8 +14,8 @@ export const Toaster = () => {
 		<Portal>
 			<ChakraToaster toaster={toaster} insetInline={{ mdDown: "4" }}>
 				{(toast) => (
-					<Toast.Root width={{ md: "sm" }} bg={"bg"}>
-						{toast.type === "loading" ? <Spinner size="sm" color="blue.solid" /> : <Toast.Indicator />}
+					<Toast.Root width={{ md: "sm" }}>
+						{toast.type === "loading" ? <Spinner size="sm" color="primary" /> : <Toast.Indicator />}
 						<Stack gap="1" flex="1" maxWidth="100%">
 							{toast.title && <Toast.Title>{toast.title}</Toast.Title>}
 							{toast.description && <Toast.Description>{toast.description}</Toast.Description>}
